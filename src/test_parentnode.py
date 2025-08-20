1f5c6b24a2c7fe79be8417262680aa293d0615d3
aadil10i <aadil3003@outlook.com>
2025-08-20 15:36:16 +0400

    parent node test cases

---
+++ src/test_parentnode.py
@@ -0,0 +1,24 @@
+import unittest
+from htmlnode import ParentNode, LeafNode
+
+
+class TestParentNodeWithChildren(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+
+class TestParentNodeWithGrandchildren(unittest.TestCase):
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
