486968e8a2f127f38af5ddd0f5b8f0d9737f4550
aadil10i <aadil3003@outlook.com>
2025-08-20 23:17:27 +0400

    textnode to leafnode converter start

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
 
-def main():
-    textnode = LeafNode("p", "hello")
-    print(textnode.to_html())
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode("", {text_node.text})
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", {text_node.text})
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", {text_node.text})
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", {text_node.text})
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", {text_node.text}, "href")
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(
+            "img",
+            "",
+            "src",
+            "alt",
+        )
 
-
-if __name__ == "__main__":
-    main()
+    else:
+        raise Exception("TextNode has none of the required types")
