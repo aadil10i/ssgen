28ff8560b58afb4a60a6481a421801d72ec0a3b2
aadil10i <aadil3003@outlook.com>
2025-08-21 18:53:46 +0400

    final textnode to htmlnode converter + it's unittests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
-
 from textnode import TextNode, TextType
+from main import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,14 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, url=None)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+
+class TestTextToHtml(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
