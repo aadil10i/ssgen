30d7f18358a22273bcbba5bbf61a500b616da47f
aadil10i <aadil3003@outlook.com>
2025-08-27 21:42:14 +0400

    created function converts markdown  string into textnodes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import TextNode, TextType
+from textnode import TextType, TextNode
 from htmlnode import LeafNode
 
 
+# convert textnodes to htmlnodes
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
@@ -17,3 +18,30 @@ def text_node_to_html_node(text_node):
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
 
     raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+# convert markdown string into textnode
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+
+        for i, section in enumerate(sections):
+            if section == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(section, TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(section, text_type))
+
+        new_nodes.extend(split_nodes)
+
+    return new_nodes
