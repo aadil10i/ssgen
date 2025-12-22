b3cf9219cb248d5e1b98d46df32734d467ddfb2e
aadil <aadil3003@outlook.com>
2025-12-22 21:38:52 +0400

    block type identifier block_to_block_type function complete

--- src/blocktype.py
+++ src/blocktype.py
@@ -1,10 +1,34 @@
 from enum import Enum, auto
 
 
-class TextType(Enum):
+class BlockType(Enum):
     PARAGRAPH = auto()
     HEADING = "#"
     CODE = "```"
     QUOTE = ">"
     UNORDERED_LIST = "*/-"
     ORDERED_LIST = "[0-9]+. "
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith(("#", "##", "###", "####", "#####", "######")):
+        return BlockType.HEADING
+
+    if block.startswith(("```")) and block.endswith(("```")):
+        return BlockType.CODE
+
+    if False not in [line.startswith(">") for line in block.split("\n")]:
+        return BlockType.QUOTE
+
+    if False not in [
+        line.startswith("*") or line.endswith("-") for line in block.split("\n")
+    ]:
+        return BlockType.UNORDERED_LIST
+
+    if False not in [
+        line.startswith("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        for line in block.split("\n")
+    ]:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
